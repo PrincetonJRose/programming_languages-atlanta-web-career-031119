b98d46e5b51d7ada9d80229e9794930c9ae1e7e5
Princeton Rose <decoyzpr@gmail.com>
2019-03-06 03:49:24 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,6 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, lang|
     lang.each do |name, type|
+      name[:style]=[]
       puts "#{name} and is #{type}"
     end
   end
