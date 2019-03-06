07eb528ac6f563f8bc35bef32550eb907a598105
Princeton Rose <decoyzpr@gmail.com>
2019-03-06 04:03:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,8 +5,10 @@ def reformat_languages(languages)
     lang.each do |name, type|
       new_hash[name] = type
       new_hash[name][:style] = []
+      if name == :javascript and style == :oo
+        new_hash[name][:style].push(style)
+      end
       new_hash[name][:style].push(style)
-      puts new_hash
     end
   end
   new_hash
