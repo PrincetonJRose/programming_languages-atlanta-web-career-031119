0325841ee6ed9b8a9aad1b1be75b671a2b43e5f2
Princeton Rose <decoyzpr@gmail.com>
2019-03-06 03:51:23 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -2,9 +2,10 @@ def reformat_languages(languages)
   # your code here
   new_hash = {}
   languages.each do |style, lang|
-    lang.collect do |name, type|
-      name[:style]=[]
+    lang.each do |name, type|
+      new_hash.push(name[:style]=[])
       puts "#{name} and is #{type}"
+      puts new_hash
     end
   end
   new_hash
